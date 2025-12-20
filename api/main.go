d2b2647f5341260497b855da1ec480fb76725656
Idan <me@idank.dev>
2025-12-20 18:00:32 +0200

    Systemd support. (#47)
    Now when starting daemon it will not be stacked in an endless loop

--- api/main.go
+++ api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"whisper-api/communication"
 	"whisper-api/config"
 	"whisper-api/endpoints"
+
+	"github.com/coreos/go-systemd/daemon"
 )
 
 func main() {
@@ -15,8 +18,18 @@ func main() {
 	router := endpoints.SetupRouter(cfg)
 	err = router.Run(cfg.Addr)
 	if err != nil {
+		if _, notifyErr := daemon.SdNotify(false, "STATUS=Service failed; ERRNO=1"); notifyErr != nil {
+			log.Printf("Failed to notify systemd: %v\n", notifyErr)
+		}
+
 		panic(err)
 	}
 
 	go communication.HandleHeartbeat(cfg)
+
+	if _, err := daemon.SdNotify(false, "READY=1"); err != nil {
+		log.Printf("Failed to notify systemd: %v\n", err)
+	} else {
+		log.Println("Notified systemd that the service is READY=1")
+	}
 }
